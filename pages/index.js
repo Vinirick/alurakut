9e2de9578267b5f6d42395b6b42be35cc955af22
Vinícius Henrique <uservinirick@gmail.com>
2021-07-15 23:41:13 -0300

    inicio-do-alurakut

---
+++ pages/index.js
@@ -0,0 +1,142 @@
+import React from "react";
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  AlurakutProfileSidebarMenuDefault,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
+
+function ProfileSideBar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        alt="Profile photo"
+        style={{ borderRadius: "8px" }}
+      />
+      <hr />
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
+    </Box>
+  );
+}
+
+export default function Home() {
+  const user = "Vinirick";
+  const [communities, setComunities] = React.useState([
+    {
+      id: "45689715",
+      title: "Eu odeio acordar cedo",
+      image: "https://alurakut.vercel.app/capa-comunidade-01.jpg",
+    },
+  ]);
+  const favoriteUsers = [
+    "juunegreiros",
+    "omariosouto",
+    "peas",
+    "rafaballerini",
+    "marcobrunodev",
+    "felipefialho",
+  ];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSideBar githubUser={user} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem vindoa)</h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                const formData = new FormData(e.target);
+                console.log(`Campo: ${formData.get("title")}`);
+
+                console.log(`Campo: ${formData.get("image")}`);
+
+                const community = {
+                  id: new Date().toISOString(),
+                  title: formData.get("title"),
+                  image: formData.get("image"),
+                };
+
+                const newArrayCommunities = [...communities, community];
+                setComunities(newArrayCommunities);
+                console.log(communities);
+              }}
+            >
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type-text="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <button>Criar Comunidade</button>
+            </form>
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <ul>
+              {communities.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <a href={`/users/${item.title}`} key={item.title}>
+                      <img src={item.image} alt={item.title} />
+                      <span>{item.title}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({favoriteUsers.length})
+            </h2>
+            <ul>
+              {favoriteUsers.map((item) => {
+                return (
+                  <li key={item}>
+                    <a href={`/users/${item}`} key={item}>
+                      <img src={`https://github.com/${item}.png`} alt={item} />
+                      <span>{item}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
+}
